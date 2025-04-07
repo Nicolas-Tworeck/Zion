9174a486bbcb00b53354f98a2d0e673a6d3a24b3
Nicolas-Tworeck <nico.tworeck@gmail.com>
2025-04-07 20:38:09 -0300

    commit

--- script/music.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const audio = new Audio('music/ambiente.mp3');
-    audio.play().catch((e) => {
-      console.warn('Autoplay bloqueado pelo navegador:', e);
-    });
-  });
