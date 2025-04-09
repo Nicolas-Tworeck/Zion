759bd01a7be3e71db17cfd8f8eab00cb8dde52a1
Nicolas-Tworeck <nico.tworeck@gmail.com>
2025-04-08 21:01:01 -0300

    commit

--- script/href.js
+++ script/href.js
@@ -1,3 +1,21 @@
 function localidade(){
     window.location.href = "https://maps.app.goo.gl/mT9im2LNxTyzyQAM6";
 }
+
+function whatsZion(){
+    window.location.href = "https://wa.link/rx30f6";
+}
+
+function emailZion(){
+    window.location.href = "mailto:at3@hotmail.com.br";
+}
+
+
+
+
+function contato(){
+    window.location.href = "contato.html";
+}
+function inicio(){
+    window.location.href = "index.html";
+}
