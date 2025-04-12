4ceab30b4c4759b71ab0d310a92c1a80120366df
Nicolas-Tworeck <nico.tworeck@gmail.com>
2025-04-11 22:39:36 -0300

    commit

--- script/href.js
+++ script/href.js
@@ -3,15 +3,36 @@ function localidade(){
 }
 
 function whatsZion(){
-    window.location.href = "https://wa.link/rx30f6";
+    window.location.href = "https://api.whatsapp.com/send?phone=5542844403228&text=Ol%C3%A1%21%20Tudo%20bem%3F%20Gostaria%20de%20solicitar%20um%20or%C3%A7amento%20para%20a%20impress%C3%A3o%20de%20um%20projeto%20com%20a%20Zion.%20Podemos%20conversar%20sobre%20os%20detalhes%3F";
 }
 
 function emailZion(){
     window.location.href = "mailto:at3@hotmail.com.br";
 }
 
+function bispo(){
+    window.location.href = "https://api.whatsapp.com/send?phone=5542999880544&text=Ol%C3%A1%2C%20Bispo%21%20Tudo%20bem%3F%20Encontrei%20a%20Zion%20pelo%20site%20e%20fiquei%20muito%20interessado%20no%20trabalho%20de%20voc%C3%AAs.%20Gostaria%20de%20solicitar%20um%20or%C3%A7amento%20para%20um%20projeto%20de%20impress%C3%A3o.%20Podemos%20conversar%20sobre%20os%20detalhes%3F";
+}
 
+function eliezer(){
+    window.location.href = "https://api.whatsapp.com/send?phone=554298690111&text=Ol%C3%A1%2C%20Eliezer%21%20Tudo%20bem%3F%20Encontrei%20a%20Zion%20atrav%C3%A9s%20do%20site%20e%20gostaria%20de%20solicitar%20um%20or%C3%A7amento%20para%20um%20projeto%20de%20impress%C3%A3o.%20Podemos%20conversar%20sobre%20os%20detalhes%3F";
+}
+function instaZion(){
+    window.location.href = "https://www.instagram.com/creality_tech/?utm_source=ig_web_button_share_sheet";
+}
 
+function siteDev(){
+    window.location.href = "https://crealitytech.com.br/";
+}
+function whatsDev(){
+    window.location.href = "https://api.whatsapp.com/send?phone=5542984033886&text=Ol%C3%A1%21%20Cheguei%20at%C3%A9%20voc%C3%AAs%20pelo%20site%20da%20Zion%20e%20gostei%20muito%20do%20trabalho.%20Gostaria%20de%20solicitar%20um%20or%C3%A7amento%20personalizado%2C%20por%20favor.";
+}
+function instaDev(){
+    window.location.href = "https://www.instagram.com/creality_tech/?utm_source=ig_web_button_share_sheet";
+}
+function mailDev(){
+    window.location.href = "mailto:contato@crealitytech.com.br";
+}
 
 function contato(){
     window.location.href = "contato.html";
