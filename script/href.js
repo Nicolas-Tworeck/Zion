1f4ddd4d6e49a253f47d5f9e7864343a4294e50a
Nicolas-Tworeck <nico.tworeck@gmail.com>
2025-04-21 14:07:49 -0300

    commit

--- script/href.js
+++ script/href.js
@@ -18,7 +18,7 @@ function eliezer(){
     window.location.href = "https://api.whatsapp.com/send?phone=554298690111&text=Ol%C3%A1%2C%20Eliezer%21%20Tudo%20bem%3F%20Encontrei%20a%20Zion%20atrav%C3%A9s%20do%20site%20e%20gostaria%20de%20solicitar%20um%20or%C3%A7amento%20para%20um%20projeto%20de%20impress%C3%A3o.%20Podemos%20conversar%20sobre%20os%20detalhes%3F";
 }
 function instaZion(){
-    window.location.href = "https://www.instagram.com/creality_tech/?utm_source=ig_web_button_share_sheet";
+    window.location.href = "https://www.instagram.com/zion.3d.industrial/?utm_source=ig_web_button_share_sheet";
 }
 
 function siteDev(){
@@ -28,10 +28,10 @@ function whatsDev(){
     window.location.href = "https://api.whatsapp.com/send?phone=5542984033886&text=Ol%C3%A1%21%20Cheguei%20at%C3%A9%20voc%C3%AAs%20pelo%20site%20da%20Zion%20e%20gostei%20muito%20do%20trabalho.%20Gostaria%20de%20solicitar%20um%20or%C3%A7amento%20personalizado%2C%20por%20favor.";
 }
 function instaDev(){
-    window.location.href = "https://www.instagram.com/creality_tech/?utm_source=ig_web_button_share_sheet";
+    window.location.href = "https://www.instagram.com/creality__tech/?utm_source=ig_web_button_share_sheet";
 }
 function mailDev(){
-    window.location.href = "mailto:contato@crealitytech.com.br";
+    window.location.href = "mailto:central@crealitytech.com.br";
 }
 
 function contato(){
